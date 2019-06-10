ae271cc7b71ff59c102933a5ea0439731b32df0c
MarkOhnsman <mark.ohnsman@gmail.com>
2019-06-10 11:26:27 -0600

    remove db

--- server/db/dbconfig.js
+++ server/db/dbconfig.js
@@ -2,7 +2,7 @@
 import mongoose from 'mongoose'
 
 //THIS STRING WILL CHANGE SLIGHTLY
-const connectionString = "mongodb://student:student@classroom-shard-00-00-afyyj.mongodb.net:27017,classroom-shard-00-01-afyyj.mongodb.net:27017,classroom-shard-00-02-afyyj.mongodb.net:27017/kanban?ssl=true&replicaSet=Classroom-shard-0&authSource=admin&retryWrites=true&w=majority"
+const connectionString = ""
 
 let connection = mongoose.connection
 
--- server/middlewear/session.js
+++ server/middlewear/session.js
@@ -2,7 +2,7 @@ import expressSession from 'express-session'
 var mongoStore = require("connect-mongodb-session")(expressSession);
 
 var store = new mongoStore({
-    uri: "mongodb://student:student@classroom-shard-00-00-afyyj.mongodb.net:27017,classroom-shard-00-01-afyyj.mongodb.net:27017,classroom-shard-00-02-afyyj.mongodb.net:27017/kanban?ssl=true&replicaSet=Classroom-shard-0&authSource=admin&retryWrites=true&w=majority", //CHANGE ME!!!!!!
+    uri: "", //CHANGE ME!!!!!!
     collection: "Sessions"
 });
 
