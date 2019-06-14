cf268814da3be2c49af5ab588f825c7d23fea281
MarkOhnsman <mark.ohnsman@gmail.com>
2019-06-14 16:01:48 -0600

    Rebuild with beauty

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -3,13 +3,13 @@
     <form v-if="loginForm" @submit.prevent="loginUser">
       <input type="email" v-model="creds.email" placeholder="email">
       <input type="password" v-model="creds.password" placeholder="password">
-      <button type="submit">Login</button>
+      <button class="btn btn-success" type="submit">Login</button>
     </form>
     <form v-else @submit.prevent="register">
       <input type="text" v-model="newUser.name" placeholder="name">
       <input type="email" v-model="newUser.email" placeholder="email">
       <input type="password" v-model="newUser.password" placeholder="password">
-      <button type="submit">Create Account</button>
+      <button class="btn btn-warning" type="submit">Create Account</button>
     </form>
     <div class="action" @click="loginForm = !loginForm">
       <p v-if="loginForm">No account? Click here to Register</p>
@@ -19,36 +19,36 @@
 </template>
 
 <script>
-  import router from '@/router.js'
-  export default {
-    name: "login",
-    data() {
-      return {
-        loginForm: true,
-        creds: {
-          email: "",
-          password: ""
-        },
-        newUser: {
-          email: "",
-          password: "",
-          name: ""
-        }
-      };
-    },
-    methods: {
-      register() {
-        this.$store.dispatch("register", this.newUser);
+import router from "@/router.js";
+export default {
+  name: "login",
+  data() {
+    return {
+      loginForm: true,
+      creds: {
+        email: "",
+        password: ""
       },
-      loginUser() {
-        this.$store.dispatch("login", this.creds);
+      newUser: {
+        email: "",
+        password: "",
+        name: ""
       }
+    };
+  },
+  methods: {
+    register() {
+      this.$store.dispatch("register", this.newUser);
+    },
+    loginUser() {
+      this.$store.dispatch("login", this.creds);
     }
-  };
+  }
+};
 </script>
 
 <style>
-  .action {
-    cursor: pointer;
-  }
+.action {
+  cursor: pointer;
+}
 </style>
