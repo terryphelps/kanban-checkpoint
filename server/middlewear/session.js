beb657b8b2c282182b8536277445a602182b9057
MarkOhnsman <mark.ohnsman@gmail.com>
2019-06-14 15:24:42 -0600

    fix session

--- server/middlewear/session.js
+++ server/middlewear/session.js
@@ -12,13 +12,15 @@ store.on("error", function (err) {
 
 
 export default class Session {
-    express = expressSession({
-        secret: "you should change this", //CHANGE ME!!!!
-        cookie: {
-            maxAge: 1000 * 60 * 60 * 24 * 7 * 52 * 2,
-        },
-        store,
-        resave: true,
-        saveUninitialized: true
-    })
+    constructor() {
+        this.express = expressSession({
+            secret: "you should change this", //CHANGE ME!!!!
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 7 * 52 * 2,
+            },
+            store,
+            resave: true,
+            saveUninitialized: true
+        })
+    }
 }
